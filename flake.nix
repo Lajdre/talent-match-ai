16b0c846bf8f550a10c794a65385d37211183245
Asterikss <AndreAndreM@proton.me>
2026-01-06 14:21:12 +0100

    chore: add the flake

---
+++ flake.nix
@@ -0,0 +1,42 @@
+{
+  description = "Talent Match AI";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    { nixpkgs, ... }:
+    let
+      inherit (nixpkgs) lib;
+      forAllSystems = lib.genAttrs lib.systems.flakeExposed;
+    in
+    {
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = pkgs.mkShell {
+            packages = [
+              pkgs.python312
+              pkgs.uv
+            ];
+
+            env = lib.optionalAttrs pkgs.stdenv.isLinux {
+              LD_LIBRARY_PATH = lib.makeLibraryPath pkgs.pythonManylinuxPackages.manylinux1;
+            };
+
+            shellHook = ''
+              unset PYTHONPATH
+              uv sync
+              . .venv/bin/activate
+              [[ -f .env ]] && set -a && source .env && set +a
+              [[ $DEVSHELL_SHELL ]] && exec "$DEVSHELL_SHELL"
+            '';
+          };
+        }
+      );
+    };
+}
