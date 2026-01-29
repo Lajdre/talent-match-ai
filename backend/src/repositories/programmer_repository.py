94cf7de51ee47fd227cfd8d510051d15707a5d98
Asterikss <AndreAndreM@proton.me>
2026-01-29 16:22:10 +0100

    refactor: change how the progammer repository fetches the programmers to fix skill duplication

--- backend/src/repositories/programmer_repository.py
+++ backend/src/repositories/programmer_repository.py
@@ -4,40 +4,35 @@ from services.neo4j_service import get_neo4j_graph
 
 def get_programmers(status: str | None = None) -> list[ProgrammerRead]:
   cypher = """
-      MATCH (p:Person)
+    MATCH (p:Person)
 
-      OPTIONAL MATCH (p)-[hs:HAS_SKILL]->(s:Skill)
+    OPTIONAL MATCH (p)-[hs:HAS_SKILL]->(s:Skill)
+    WITH p, collect({
+      skill: s.id,
+      proficiency: hs.proficiency
+    }) AS raw_skills
 
-      OPTIONAL MATCH (p)-[:ASSIGNED_TO]->(proj:Project)
-      WHERE proj.status IN ['active', 'planned']
+    OPTIONAL MATCH (p)-[:ASSIGNED_TO]->(proj:Project)
+    WHERE proj.status IN ['active', 'planned']
+    WITH
+      p,
+      raw_skills,
+      collect(DISTINCT proj.title) AS active_projects
 
-      WITH
-        p,
-        collect({
-          skill: s.id,
-          proficiency: hs.proficiency
-        }) AS raw_skills,
-        collect(DISTINCT proj.title) AS active_projects
-
-      WITH
-        p,
-        {
-          Expert: [x IN raw_skills WHERE x.proficiency = 'Expert' | x.skill],
-          Advanced: [x IN raw_skills WHERE x.proficiency = 'Advanced' | x.skill],
-          Intermediate: [x IN raw_skills WHERE x.proficiency = 'Intermediate' | x.skill],
-          Beginner: [x IN raw_skills WHERE x.proficiency = 'Beginner' | x.skill]
-        } AS skills_by_level,
-        active_projects
-
-      RETURN {
-        id: p.id,
-        name: p.name,
-        location: p.location,
-        skills: skills_by_level,
-        is_assigned: size(active_projects) > 0,
-        current_project: head(active_projects)
-      } AS data
-    """
+    RETURN {
+      id: p.id,
+      name: p.name,
+      location: p.location,
+      skills: {
+        Expert: [x IN raw_skills WHERE x.proficiency = 'Expert' | x.skill],
+        Advanced: [x IN raw_skills WHERE x.proficiency = 'Advanced' | x.skill],
+        Intermediate: [x IN raw_skills WHERE x.proficiency = 'Intermediate' | x.skill],
+        Beginner: [x IN raw_skills WHERE x.proficiency = 'Beginner' | x.skill]
+      },
+      is_assigned: size(active_projects) > 0,
+      current_project: head(active_projects)
+    } AS data
+  """
 
   results = get_neo4j_graph().query(cypher)
   parsed_results = [ProgrammerRead(**row["data"]) for row in results]
